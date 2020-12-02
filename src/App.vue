ffde3e0cb1e33e1004bfee52e5e0a282660a0ad1
Ogunwobi Philip <paogunwobi@gmail.com>
2020-12-02 15:51:48 +0100

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
+$image-path: "~@/../mdb/mdbvue/img";
+@import "~@/../mdb/mdbvue/scss/mdb-free.scss";
+
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  font-weight: 400;
 }
 </style>
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,6 @@
 declare module '*.vue' {
+  // eslint-disable-next-line prettier/prettier
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<{}, {}, unknown>
   export default component
 }
